8ba73c758d2e8f15c610e9be70ab45cb0ac661c5
esh <esh.eburg@gmail.com>
2020-08-25 18:41:39 +0300

    1) filetypes.cpp: added commonword to keywords (similarly in C lang) 2) filetypes.go: added commonword to keywords (similarly in C lang)

--- data/filedefs/filetypes.go
+++ data/filedefs/filetypes.go
@@ -4,11 +4,12 @@
 
 [keywords]
 # all items must be in one line
-primary=break case chan const continue default defer else fallthrough for func go goto if import interface map package range return select struct switch type var true false iota nil
+primary=break case chan const continue default defer else fallthrough for func go goto if import interface map package range return select struct switch type var iota
 secondary=bool byte complex64 complex128 error float32 float64 int int8 int16 int32 int64 rune string uint uint8 uint16 uint32 uint64 uintptr
-
 # these are the Doxygen keywords
 docComment=a addindex addtogroup anchor arg attention author authors b brief bug c callergraph callgraph category cite class code cond copybrief copydetails copydoc copyright date def defgroup deprecated details dir dontinclude dot dotfile e else elseif em endcode endcond enddot endhtmlonly endif endinternal endlatexonly endlink endmanonly endmsc endrtfonly endverbatim endxmlonly enum example exception extends file fn headerfile hideinitializer htmlinclude htmlonly if ifnot image implements include includelineno ingroup interface internal invariant latexonly li line link mainpage manonly memberof msc mscfile n name namespace nosubgrouping note overload p package page par paragraph param post pre private privatesection property protected protectedsection protocol public publicsection ref related relatedalso relates relatesalso remark remarks result return returns retval rtfonly sa section see short showinitializer since skip skipline snippet struct subpage subsection subsubsection tableofcontents test throw throws todo tparam typedef union until var verbatim verbinclude version warning weakgroup xmlonly xrefitem
+# Common keywords
+commonword=nil true false
 
 [lexer_properties=C]
 lexer.cpp.backquoted.strings=1
