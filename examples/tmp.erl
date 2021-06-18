9a05194dce417ec04f544fc80628c0eed7619ea0
esh <esh.eburg@gmail.com>
2021-06-18 09:55:48 +0300

    1) added valid erlang code examples with problem highlighting (in geany) 2) refactored and improved erlang lexer

--- examples/tmp.erl
+++ examples/tmp.erl
@@ -1,46 +1,105 @@
--module(tmpmodule).
--define
--defin
+-module(tmp).
+-compile(nowarn_export_all).
+-compile(export_all).
 
-io:format("test ~s~n~c\n~5f\n~5.7f\n~5.*f\n~-5f\n~ts~lp\n~n",
-		  ["test", 1, 2, 3.5, 2, 78, 0.12]).
+-define(CONFIG_MODULE, server_config).
+-define(CODE_VERSION, "1.4").
+-define(CALL@_TIMEOUT, 1000).
+%~ -define
+%~ -defin
 
-TEST1 = "\ \sx\tx\nx\rxx\"x\'x\!x\@x\#x\$x\%x\6x".
-TEST2 = "\u00d0x\u00a8x\u00d0x\u00b0x\u00d0x\u00b1x\u00d0x\u00bbx\u00d0".
-TEST3 = <<"\u00d0x\u00a8x\u00d0x\u00b0x\u00d0x\u00b1x\u00d0x\u00bbx\u00d0">>.
-TEST4 = <<"test123\312546\76875\547sad">>.
+-record('RuntimeInfo', {
+  duration,
+  startedAt,
+  medias,
+  sessions,
+  inMediaBandwidth,
+  inSystemBandwidth,
+  outMediaBandwidth,
+  outSystemBandwidth,
+  cpu,
+  memory,
+  %% with exensions
+  erlang,
+  schedulerLoad,
+  schedulerLoadNormal,
+  schedulerLoadDCPU,
+  schedulerLoadDIO
+}).
 
-Var01 = <<"1.0000000000000000000000001">>.
-Var02 = 2.300000 = 2.30000e+0 = 2.3.
-Var03 = 1.1+1.2 = 1.1 + 1.2 = 2.3.
-Var04 = 1.1-1.2 = 1.1 - 1.2 = -0.09999999999999987.
-Var05 = 0.123456789e-100 * 0.123456789e-100 = 1.524157875019052e-202.
-Var06 = 2.30000e-10 = 2.3e-10.
-Var07 = 2.30000e+10 = 2.3e10.
-Var08 = 2.30000e-1 = 2.30000e-01 = 2.30000e-001 = 0.23.
-Var09 = 2.30000e+1 = 2.30000e+01 = 2.30000e+001 = 23.0.
-Var10 = X + Y = X+Y.
-Var11 = X - Y = X-Y.
-Var12 = 0.123456789e-1 = 0.123456789 / 10 = 0.0123456789.
-Var13 = 0.123456789e-2 = 0.123456789 / 100 = 0.00123456789.
-Var14 = 0.123456789e+1 = 0.123456789e1 = 0.123456789 * 10 = 1.23456789.
-Var15 = 0.123456789e+2 = 0.123456789e2 = 0.123456789 * 100 = 12.3456789.
-Var16 = +0.123 = 0+0.123 = 0.123.
-Var17 = -0.123 = 0-0.123 = -0.123.
-Var18 = -0.1e+0 = -0.1e+00.
-Var19 = 1+1-2 = 1 + 1 - 2.
+tmp() ->
+	io:format("test ~s~n~c\n~5f\n~5.7f\n~5.*f\n~-5f\n~ts~lp\n~n",
+			  ["test", $a, 2, 3.5, 2.3e-10, 78, $\377, 5, 2]),
 
-+
-++
--
---
+	_TEST1 = "\ \sx\tx\nx\rxx\"x\'x\!x\@x\#x\$x\%x\6x",
+	_TEST2 = "\u00d0x\u00a8x\u00d0x\u00b0x\u00d0x\u00b1x\u00d0x\u00bbx\u00d0",
+	_TEST3 = <<"\u00d0x\u00a8x\u00d0x\u00b0x\u00d0x\u00b1x\u00d0x\u00bbx\u00d0">>,
+	_TEST4 = <<"test123\312546\76875\547sad">>,
+
+	X = 15,
+	Y = 20,
+	_Tmp01 = X + Y,
+	_Tmp02 = X - Y,
+	_Tmp03 = X+Y,
+	_Tmp04 = X-Y,
+	
+	_Var01 = <<"1.0000000000000000000000001">>,
+	_Var02 = 2.300000 = 2.30000e+0 = 2.3,
+	_Var03 = 1.1+1.2 = 1.1 + 1.2 = 2.3,
+	_Var04 = 1.1-1.2 = 1.1 - 1.2 = -0.09999999999999987,
+	_Var05 = 0.123456789e-100 * 0.123456789e-100 = 1.524157875019052e-202,
+	_Var06 = 2.30000e-10 = 2.3e-10,
+	_Var07 = 2.30000e+10 = 2.3e10,
+	_Var08 = 2.30000e-1 = 2.30000e-01 = 2.30000e-001 = 0.23,
+	_Var09 = 2.30000e+1 = 2.30000e+01 = 2.30000e+001 = 23.0,
+	_Var10 = 0.123456789e-1 = 0.123456789 / 10 = 0.0123456789,
+	_Var11 = 0.123456789e-2 = 0.123456789 / 100 = 0.00123456789,
+	_Var12 = 0.123456789e+1 = 0.123456789e1 = 0.123456789 * 10 = 1.23456789,
+	_Var13 = 0.123456789e+2 = 0.123456789e2 = 0.123456789 * 100 = 12.3456789,
+	_Var14 = +0.123 = 0+0.123 = 0.123,
+	_Var15 = -0.123 = 0-0.123 = -0.123,
+	_Var16 = -0.1e+0 = -0.1e+00,
+	_Var17 = 1+1-2 = 1 + 1 - 2,
+	
+	%~ _Var18 = 16#9g, % invalid hex
+	_Var19 = 16#91, % valid hex
+	_Var20 = 16#9f, % valid hex
+	
+	%~ _Var21 = 10#9f, % invalid dec
+	_Var22 = 10#99, % valid dec
+	
+	%~ _Var23 = 8#78,  % invalid oct
+	_Var24 = 8#77,  % valid oct
+	
+	%~ _Var25 = 2#12,  % invalid bin
+	_Var26 = 2#11.  % valid bin
+
+%~ +
+%~ ++
+%~ -
+%~ --
+
+tmp2(Body, Major, Minor, Patch, Build, Mod, Socket, Request) ->
+	
+	_Val1 = io_lib:format("~2..0B~2..0B~2..0B~2..0B", [Major, Minor, Patch, Build]),
+	_Val2 = [ Y || <<_:16, Y:16/unsigned-little-integer>> <= Body ],
+	<<_UnitId:8, UnitSize:8, _Unit0:UnitSize/binary, _Rest/binary>> = _Val2,
+	
+	ok = Mod:send(Socket, Request).		% this is a not bif-func
+
+default() -> ok.
+yield() -> ok.
 
 test(Var) ->
-	throw:123 = Val -> max(1, Val)			% this is a bif-func
+	try throw(123)
+	catch
+		throw:123 = Val -> max(1, Val) % this is a bif-func
+	end,
 	_V11 = max(1,3),						% this is a bif-func
 	_V12 = erlang:max(1,3),					% this is a bif-func
 	_V13 = test:max(1,3),					% this is a not bif-func
 	_V14 = 'erlang':max(1,3),				% this is a not bif-func
+	Mod = ?MODULE,
 	_V15 = Mod:max(1,3),					% this is a not bif-func
 	_V21 = yield(),							% this is a bif-func
 	_V22 = erlang:yield(),					% this is a bif-func
@@ -51,6 +110,7 @@ test(Var) ->
 	_V51 = os_stat_mem:'L6Proto'(Var),
 	_V52 = os_stat_mem:'L6Proto'(test_atom),
 	_V53 = server_config:'$mapper_record'(tc_decoder),
+	Bin = <<"test">>,
 	_V61 = 'L6proto':decode('PresenceMessage', Bin),
 	_V62 = 'L6proto':'decode'('PresenceMessage', Bin),
 	_V71 = nonode@nohost,
@@ -61,14 +121,12 @@ test(Var) ->
 	Param.
 
 info() ->
+  Info = #'RuntimeInfo'{duration = 35, startedAt = 2, medias = 7, sessions = 78, cpu = 16, memory = 2048},
+  
   Info#'RuntimeInfo'.duration - 0 > 0 orelse error([bad_duration,Info#'RuntimeInfo'.duration]),
   Info#'RuntimeInfo'.startedAt - 1600000000 > 0 andalso Info#'RuntimeInfo'.startedAt - 1800000000 < 0 orelse error([bad_startedAt,Info#'RuntimeInfo'.startedAt]),
   Info#'RuntimeInfo'.medias - 0 > 0 orelse error([bad_medias,Info#'RuntimeInfo'.medias]),
   Info#'RuntimeInfo'.sessions - 0 > 0 orelse error([bad_sessions,Info#'RuntimeInfo'.sessions]),
-  % Info#'RuntimeInfo'.inMediaBandwidth - 0 > 0 orelse error([bad_inMediaBandwidth,Info#'RuntimeInfo'.inMediaBandwidth]),
-  % Info#'RuntimeInfo'.outMediaBandwidth - 0 > 0 orelse error([bad_outMediaBandwidth,Info#'RuntimeInfo'.outMediaBandwidth]),
-  % Info#'RuntimeInfo'.inSystemBandwidth - 0 > 0 orelse error([bad_inSystemBandwidth,Info#'RuntimeInfo'.inSystemBandwidth]),
-  % Info#'RuntimeInfo'.outSystemBandwidth - 0 > 0 orelse error([bad_outSystemBandwidth,Info#'RuntimeInfo'.outSystemBandwidth]),
   Info#'RuntimeInfo'.cpu - 0 > 0 orelse error([bad_cpu,Info#'RuntimeInfo'.cpu]),
   Info#'RuntimeInfo'.memory - 0 > 0 orelse error([bad_memory,Info#'RuntimeInfo'.memory]),
   ok.
@@ -91,6 +149,8 @@ call(Call) ->
   end.
 
 domain4(<<":", I, _/binary>>, Acc) when I >= $0 andalso I =< $9 orelse I == $/ -> Acc.
+
+tmp3(S1, Options) ->
 	[{<<"v2">>,
 	  #{data:= <<_, $P, $N, $G, _/binary>>,
 		path:= <<"@logo.png">>,
@@ -107,6 +167,8 @@ config(<<C, Rest/binary>>, L, R) ->
 			 what => <<C>>,
 			 rest => Rest}}].
 
+params(_V1, _V2, _V3, _V4) -> ok.
+
 command(<<C, Bin/binary>>, L, R0, R, Acc)
 		when C == $\t orelse C == $\r orelse C == $\s -> 
   [{command, L, R0, Acc} | params(Bin, L, R+1, Acc == <<"url">> orelse
@@ -116,7 +178,7 @@ command(<<C, Bin/binary>>, L, R0, R, Acc)
 											   Acc == <<"backend">> orelse
 											   Acc == <<"push">> orelse
 											   Acc == <<"sink">> orelse
-											   Acc == <<"auth">>)];
+											   Acc == <<"auth">>)].
 
 quoted_string(<<"\n", Bin/binary>>, L0, R0, L, _R, Acc) ->
 	quoted_string(Bin, L0, R0, L+1, 1, <<Acc/binary,"\n">>);
@@ -128,38 +190,37 @@ quoted_string(<<$\\,$\\, Bin/binary>>, L0, R0, L, R, Acc) ->
 to_int3(<<I, Bin/binary>>, Base, Num) when I >= $0 andalso I =< $9 ->
   to_int3(Bin, Base, Num*Base + I - $0).
 
+activate_from_file0() -> ok.
 activate_from_file() ->
   try activate_from_file0()
   catch
     throw:R -> R
   end.
 
-Val1 = [ Y || <<_:16, Y:16/unsigned-little-integer>> <= Body ].
-Val2 = <<UnitId:8, UnitSize:8, Unit0:UnitSize/binary, Rest/binary>>.
-Val3 = io_lib:format("~2..0B~2..0B~2..0B~2..0B", [Major, Minor, Patch, Build]).
-
-ok = Mod:send(Socket, Request).		% this is a not bif-func
-
 dec_FastStartToken(Bytes) ->
-'H235-SECURITY-MESSAGES':dec_ClearToken(Bytes).
+	'H235-SECURITY-MESSAGES':dec_ClearToken(Bytes).
 
 'enc_FastStartToken'(Val) ->
-  'H235-SECURITY-MESSAGES':'enc_ClearToken'(Val).
+	'H235-SECURITY-MESSAGES':'enc_ClearToken'(Val).
 
-case Module:'$mapper_record'(RecName) of
-  undefined -> undefined;
-  Oth -> Oth
-end,
-
-case ?CONFIG_MODULE:'$mapper_type'(TName) of
-  undefined -> undefined;
-  Oth -> Oth
-end,
-
-Records = ?MODULE:'$mapper_records'().
+tmp4(Module, RecName, TName) ->
+	case Module:'$mapper_record'(RecName) of
+	  undefined -> undefined;
+	  Oth1 -> Oth1
+	end,
+	
+	case ?CONFIG_MODULE:'$mapper_type'(TName) of
+	  undefined -> undefined;
+	  Oth2 -> Oth2
+	end,
+	
+	_Records = ?MODULE:'$mapper_records'().
 
 capabilities(Cs) ->
   lists:foldl(fun erlang:'bor'/2, 0, Cs).
+
+yeccpars1(_Tokens, _Tzr, _State, _States, _Vstack) -> ok.
+yecc_error_type(_Error, _Stacktrace) -> ok.
 
 yeccpars0(Tokens, Tzr, State, States, Vstack) ->
     try yeccpars1(Tokens, Tzr, State, States, Vstack)
@@ -175,3 +236,11 @@ yeccpars0(Tokens, Tzr, State, States, Vstack) ->
         throw: {error, {_Line, ?MODULE, _M}} = Error ->
             Error
     end.
+
+add_conf(Stream, Acc) ->
+  case application:get_env(config2, {fake_backend, Stream}) of
+    {ok, ignore} -> Acc;
+    {ok, #{} = Conf} -> Acc#{Stream => Conf};
+    {ok, null} -> Acc#{Stream => null};
+    undefined -> Acc#{Stream => null}
+  end.
--- examples/tmp2.erl
+++ examples/tmp2.erl
@@ -1,6 +1,17 @@
 -module(tmp2).
+-compile(nowarn_export_all).
+-compile(export_all).
 
--export([test1/0, test2/0, test3/0, test4/0, test5/0, test6/1, test7/0]).
+-record(rmq@obj_, {f1@1, f1_2}).
+-record(rmq@obj@, {f21@, f22_}).
+-record('@rmq@obj@', {f21@, f22_}).
+
+-define(CALL@_TIMEOUT, 1000).
+-define(CALL_TIMEOUT@, 2000).
+-define('call@_timeout', 3000).
+%~ -define(call@_timeout, 3000). % equal 'call@_timeout'
+-define('@call_timeout@', 4000).
+-define(call@_timeout@, 5000).
 
 %~ tmp2:test1().
 test1() ->
@@ -11,9 +22,10 @@ test1() ->
 
 %~ tmp2:test2().
 test2() ->
-  try throw(test_err2)
+  try throw(test_@err2@)
   catch
-    throw:test_err2 = Val -> Val
+    throw:test_@err2@ = Val ->
+      io:format("~n~p is atom = ~p~n", [Val, is_atom(Val)])
   end.
 
 %~ tmp2:test3().
@@ -46,7 +58,79 @@ test6(Var) ->
 
 %~ tmp2:test7().
 test7() ->
-  try throw("test_err7")
+  try throw("test\n_
+             err7")
   catch
-    throw:"test_err7" = Val -> Val
+    throw:V@al -> V@al
   end.
+
+%~ tmp2:test8().
+test8() ->
+  try throw('test\n_
+             err8') % edit this line fails highligting (fixed in this commit)
+  catch
+    throw:_@al ->
+      io:format("~n~p is atom = ~p~n", [_@al, is_atom(_@al)])
+  end.
+
+%~ tmp2:test9().
+test9() ->
+  try throw('tes@t\n_
+             err9')
+  catch
+    throw:_@al ->
+      io:format("~n~p is atom = ~p~n", [_@al, is_atom(_@al)])
+  end.
+
+%~ tmp2:test10().
+test10() ->
+  try throw('@tes@t\n_
+             err10')
+  catch
+    throw:_@al@ ->
+      io:format("~n~p is atom = ~p~n", [_@al@, is_atom(_@al@)])
+  end.
+
+%~ tmp2:test11().
+test11() ->
+  Node1 = node@test,
+  Node2 = 'node@test',
+  Node3 = '@test',
+  Atom1 = node@te@st,
+  Atom2 = 'node@te@st',
+  Atom3 = '@te@st',
+  Record1 = #rmq@obj_{f1@1 = val_1, f1_2 = val@2},
+  Record2 = #rmq@obj@{f21@ = val_1, f22_ = val@2},
+  Record3 = # rmq@obj_{f1@1 = val_1, f1_2 = val@2},
+  Record4 = #	rmq@obj@{f21@ = val_1, f22_ = val@2},
+  Record5 = #'@rmq@obj@'{f21@ = val_1, f22_ = val@2},
+  Record6 = # '@rmq@obj@'{f21@ = val_1, f22_ = val@2},
+  Record7 = #	'@rmq@obj@'{f21@ = val_1, f22_ = val@2},
+  io:format("~p is atom = ~p~n", [Node1, is_atom(Node1)]),
+  io:format("~p is atom = ~p~n", [Node2, is_atom(Node2)]),
+  io:format("~p is atom = ~p~n", [Node3, is_atom(Node3)]),
+  io:format("~p is atom = ~p~n", [Atom1, is_atom(Atom1)]),
+  io:format("~p is atom = ~p~n", [Atom2, is_atom(Atom2)]),
+  io:format("~p is atom = ~p~n", [Atom3, is_atom(Atom3)]),
+  io:format("Macro1 = ~p~n", [?CALL@_TIMEOUT]),
+  io:format("Macro2 = ~p~n", [?CALL_TIMEOUT@]),
+  io:format("Macro2 = ~p~n", [? CALL_TIMEOUT@]),
+  io:format("Macro2 = ~p~n", [?	CALL_TIMEOUT@]),
+  io:format("Macro3 = ~p~n", [?'call@_timeout']),
+  io:format("Macro3 = ~p~n", [? 'call@_timeout']),
+  io:format("Macro3 = ~p~n", [?	'call@_timeout']),
+  io:format("Macro4 = ~p~n", [?'@call_timeout@']),
+  io:format("Macro4 = ~p~n", [? '@call_timeout@']),
+  io:format("Macro4 = ~p~n", [?	'@call_timeout@']),
+  io:format("Macro5 = ~p~n", [?call@_timeout@]),
+  io:format("Macro5 = ~p~n", [? call@_timeout@]),
+  io:format("Macro5 = ~p~n", [?	call@_timeout@]),
+  io:format("Record1 = ~p~n", [Record1]),
+  io:format("Record2 = ~p~n", [Record2]),
+  io:format("Record3 = ~p~n", [Record3]),
+  io:format("Record4 = ~p~n", [Record4]),
+  io:format("Record5 = ~p~n", [Record5]),
+  io:format("Record6 = ~p~n", [Record6]),
+  io:format("Record7 = ~p~n", [Record7]),
+  io:format("Record7.f21@ = ~p~n", [Record7#'@rmq@obj@'.f21@]),
+  ok.
