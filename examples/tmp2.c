81eaae66081cc66c435447e6429032e80accaa9d
esh <esh.eburg@gmail.com>
2025-06-10 00:45:48 +0300

    fixed syntax highlighting with backslash in LexCPP (added examples)

---
+++ examples/tmp2.c
@@ -0,0 +1,29 @@
+// TODO
+\\
+
+// this backslash:
+\
+		 var12 = "1";
+		 var12 = "2";
+
+		 part3";
+
+var12 = "part1 \n
+		 part2 \\
+		 part3";
+
+// this 3 backslashes:
+\
+\
+\
+v = 1 + 2;
+
+
+// this line comment with backslashes (in fact it becomes a multi-line comment):
+//sdfs\
+	sdfs/\
+	\
+this expression:
+v = 1 + 2;
+
+\
