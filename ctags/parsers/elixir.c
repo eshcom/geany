cfddeb00532123036de2446a14476fd856749e8f
esh <esheburg@gmail.com>
2026-05-13 19:40:31 +0300

    1) added SCE_ELIXIR_USR_WORD style, added logic to set this style 2) expanded keywords for std_modules and lib_macros in the Elixir-lexer config 3) improved the tag-parser for Elixir-code

--- ctags/parsers/elixir.c
+++ ctags/parsers/elixir.c
@@ -631,7 +631,7 @@ static const unsigned char *parseKeyword(const unsigned char *cp, int indent)
 		cp = parseMemberTag(cp, K_FUNCTION, true, false);
 	else if (strcmp(kwval, "defmemo") == 0)
 		cp = parseMemberTag(cp, K_FUNCTION, false, false);
-	else if (strncmp(kwval, "def", 3) == 0) // def... - user-defined macro
+	else if (strncmp(kwval, "def", 3) == 0) // def... - user-defined keyword
 		cp = parseMemberTag(cp, K_FUNCTION, false, false);
 	else if (strcmp(kwval, "@typep") == 0)
 		cp = parseMemberTag(cp, K_TYPE, true, false);
