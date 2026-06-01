918d1d6378e44a72a8f8830f72ec281825b903af
esh <esheburg@gmail.com>
2026-06-01 16:45:11 +0300

    expanded standard keywords for C/C++ lexers

--- data/filedefs/filetypes.c
+++ data/filedefs/filetypes.c
@@ -46,7 +46,7 @@ preproccommentdoc=comment_doc
 
 [keywords]
 # Standard keywords
-std_words=_Alignas _Alignof _Atomic _Bool _Complex _Generic _Imaginary _Noreturn _Static_assert _Thread_local asm auto bool break case char const continue default do double else enum extern float for goto if inline int long register restrict return short signed sizeof static struct switch typedef union unsigned void volatile while
+std_words=_Alignas _Alignof _Atomic _Bool _Complex _Generic _Imaginary _Noreturn _Static_assert _Thread_local asm auto bool break case char char8_t char16_t char32_t const continue default do double else enum extern float for goto if inline int long register restrict return short signed sizeof static struct switch typedef union unsigned void volatile wchar_t while
 # Additional keywords
 add_words=
 # Common keywords
