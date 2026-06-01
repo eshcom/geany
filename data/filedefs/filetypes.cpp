31f1c508a75a1af78ebebd86d2c31b6847b87b7e
esh <esheburg@gmail.com>
2026-06-01 16:45:11 +0300

    expanded standard keywords for C/C++ lexers

--- data/filedefs/filetypes.cpp
+++ data/filedefs/filetypes.cpp
@@ -3,7 +3,7 @@
 
 [keywords=C]
 # Standard keywords
-std_words=alignas alignof and and_eq asm auto bitand bitor bool break case catch char char16_t char32_t class compl const const_cast constexpr continue decltype default delete do double dynamic_cast else enum explicit export extern final float for friend goto if inline int long mutable namespace new noexcept not not_eq operator or or_eq override private protected public register reinterpret_cast return short signed sizeof static static_assert static_cast struct switch template this thread_local throw try typedef typeid typename union unsigned using virtual void volatile wchar_t while xor xor_eq
+std_words=alignas alignof and and_eq asm auto bitand bitor bool break case catch char char8_t char16_t char32_t class compl const const_cast constexpr continue decltype default delete do double dynamic_cast else enum explicit export extern final float for friend goto if inline int long mutable namespace new noexcept not not_eq operator or or_eq override private protected public register reinterpret_cast return short signed sizeof static static_assert static_cast struct switch template this thread_local throw try typedef typeid typename union unsigned using virtual void volatile wchar_t while xor xor_eq
 # Common keywords (C-com_words + nullptr)
 com_words=TRUE FALSE NULL NAN EOF WEOF true false noreturn stderr stdin stdout nullptr
 
