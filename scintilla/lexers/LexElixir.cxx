c7fbbbd7bbc37db8946d4b81a1bcc83866f73428
esh <esheburg@gmail.com>
2026-07-29 16:42:17 +0300

    fixed syntax highlighting in Elixir code, example:
    if String.length(System.get_env("KAFKA_ENDPOINTS", "")) > 0 do
      config :df, DfAsync.KafkaEntrypoint, enabled: true
    else
      config :df, DfAsync.KafkaEntrypoint, enabled: false
    end

--- scintilla/lexers/LexElixir.cxx
+++ scintilla/lexers/LexElixir.cxx
@@ -886,7 +886,8 @@ void SCI_METHOD LexerElixir::Lex(Sci_PositionU startPos, Sci_Position length,
 		if (last_state == SCE_ELIXIR_STD_WORD) {
 			while (back > 0 && styler.StyleAt(back - 1) == SCE_ELIXIR_STD_WORD)
 				back--;
-			if (styler.Match(back, "do") || styler.Match(back, "end"))
+			if (styler.Match(back, "do") || styler.Match(back, "end")
+				|| styler.Match(back, "else"))
 				last_state = SCE_ELIXIR_DEFAULT;
 		}
 	}
@@ -1401,8 +1402,9 @@ void SCI_METHOD LexerElixir::Lex(Sci_PositionU startPos, Sci_Position length,
 				module_type = NONE_MODULE;
 				last_state = (sc.state == SCE_ELIXIR_STD_WORD &&
 							  (strcmp(ident, "do") == 0 ||
-							   strcmp(ident, "end") == 0)) ? SCE_ELIXIR_DEFAULT
-														   : sc.state;
+							   strcmp(ident, "end") == 0 ||
+							   strcmp(ident, "else") == 0)) ? SCE_ELIXIR_DEFAULT
+															: sc.state;
 				sc.SetState(SCE_ELIXIR_DEFAULT);
 			} break;
 			
